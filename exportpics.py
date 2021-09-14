2dcb4151ac0df33036b7251a1105088b2a2bd0d5
BioHazard <3482991796@qq.com>
2021-09-14 21:48:30 +0800

    增加参数heading、note圆点抗锯齿
    增加参数heading，可以自定义标题文字
    增加note圆点抗锯齿，但这会大大增加程序运行时间
    （因为pil不内置抗锯齿功能，通过增加分辨率之后缩小图片实现的抗锯齿）
    有办法优化吗？

--- exportpics.py
+++ exportpics.py
@@ -26,6 +26,11 @@ PITCH_TO_MBNUM = {93: 29, 91: 28, 89: 27, 88: 26, 87: 25, 86: 24, 85: 23, 84: 22
                   83: 21, 82: 20, 81: 19, 80: 18, 79: 17, 78: 16, 77: 15, 76: 14,
                   75: 13, 74: 12, 73: 11, 72: 10, 71: 9, 70: 8, 69: 7, 67: 6,
                   65: 5, 64: 4, 62: 3, 60: 2, 55: 1, 53: 0}
+
+LEFT_ALIGN = 0
+CENTER_ALIGN = 1
+RIGHT_ALIGN = 2
+
 A4_VERTICAL = 25
 A4_HORIZONAL = 24
 A3_VERTICAL = 23
@@ -35,6 +40,7 @@ B5_HORIZONAL = 42
 B4_VERTICAL = 41
 B4_HORIZONAL = 40
 AUTO_SIZE = 0
+
 PAPER_INFO = {A4_VERTICAL: {'size': (210, 297), 'col': 3, 'row': 35},
               A4_HORIZONAL: {'size': (297, 210), 'col': 4, 'row': 24},
               A3_VERTICAL: {'size': (297, 420), 'col': 4, 'row': 50},
@@ -56,6 +62,7 @@ INCH_TO_MM = 25.4  # 1英寸=25.4毫米
 
 DOT_R = 1.14  # 圆点半径（单位毫米）
 BORDER = 3.0  # 边框宽度（单位毫米）
+ANTI_ALIAS = 1.5  # 抗锯齿缩放倍数
 
 
 def _find_latest_event(l, t):
@@ -65,18 +72,32 @@ def _find_latest_event(l, t):
     return i
 
 
+def mm2pixel(x, ppi=DEFAULT_PPI):
+    return x / INCH_TO_MM * ppi
+
+
+def pixel2mm(x, ppi=DEFAULT_PPI):
+    return x * INCH_TO_MM / ppi
+
+
+def posconvert(pos, ppi=DEFAULT_PPI):
+    x, y = pos
+    return (round(mm2pixel(x, ppi) - 0.5), round(mm2pixel(y, ppi) - 0.5))
+
+
 def export_pics(file,
                 filename: str = None,
                 musicname: str = None,
+                transposition: int = 0,
+                interpret_bpm: float = None,
+                scale: float = 1.0,
+                heading: tuple = ('', CENTER_ALIGN),
+                font: str = None,
                 papersize=A4_VERTICAL,
                 ppi: float = DEFAULT_PPI,
                 background=(255, 255, 255, 255),
                 save_pic: bool = True,
-                overwrite: bool = False,
-                font: str = None,
-                transposition: int = 0,
-                interpret_bpm: float = None,
-                scale: float = 1.0) -> list:
+                overwrite: bool = False) -> list:
     '''
     将.emid或.mid文件转换成纸带八音盒设计稿
 
@@ -85,6 +106,21 @@ def export_pics(file,
                     例如：'MusicName_%d.png'
                     留空则取参数file的文件名+'_%d.png'
     参数 musicname: 每栏右上角的信息，留空则取参数file的文件名
+    参数 transposition: 转调，表示升高的半音数，默认为0（不转调）
+    参数 interpret_bpm: 设定此参数会使得note圆点的纵向间隔随着midi的bpm的变化而变化，
+                        note圆点间隔的缩放倍数 = interpret_bpm / midi的bpm，
+                        例如，midi的bpm被设定为75，interpret_bpm设定为100，
+                        则note圆点的间隔拉伸为4/3倍，
+                        设置为None则忽略midi的bpm信息，固定1拍=8毫米间隔，
+                        默认为None
+    参数 scale: 音符位置的缩放量，大于1则拉伸纸带长度，默认为1（不缩放）
+    参数 heading: 一个元组，
+                heading[0]: 标题文字字符串，
+                heading[1]: exportpics.LEFT_ALIGN 或
+                            exportpics.CENTER_ALIGN 或
+                            exportpics.RIGHT_ALIGN，指定对齐方式
+    参数 font: 用字符串表示的字体文件路径，
+                留空则从FONT_PATH中按序取用
     参数 papersize: 字符串或字典
                 可以使用PAPER_INFO中的预设值(例如exportpics.A4_VERTICAL)，
                 也可以使用字典来自定义，格式为
@@ -102,29 +138,9 @@ def export_pics(file,
     参数 save_pic: True 或 False，是否将图片写入磁盘，默认为True
     参数 overwrite: True 或 False，是否允许覆盖同名文件，默认为False，
                     警告：设置为True可能导致原有文件丢失，请注意备份！
-    参数 font: 用字符串表示的字体文件路径，
-                留空则从FONT_PATH中按序取用
-    参数 transposition: 转调，表示升高的半音数，默认为0（不转调）
-    参数 interpret_bpm: 设定此参数会使得note圆点的纵向间隔随着midi的bpm的变化而变化，
-                        note圆点间隔的缩放倍数 = interpret_bpm / midi的bpm，
-                        例如，midi的bpm被设定为75，interpret_bpm设定为100，
-                        则note圆点的间隔拉伸为4/3倍，
-                        设置为None则忽略midi的bpm信息，固定1拍=8毫米间隔，
-                        默认为None
-    参数 scale: 音符位置的缩放量，大于1则拉伸纸带长度，默认为1（不缩放）
 
     函数返回包含若干PIL.Image.PIL.Image实例的list
     '''
-    def mm2pixel(x, ppi=ppi):
-        return x / INCH_TO_MM * ppi
-
-    def pixel2mm(x, ppi=ppi):
-        return x * INCH_TO_MM / ppi
-
-    def posconvert(pos, ppi=ppi):
-        x, y = pos
-        return (round(mm2pixel(x, ppi) - 0.5),
-                round(mm2pixel(y, ppi) - 0.5))
 
     def process_emidfile(emidfile, transposition=transposition):
         '处理emid文件'
@@ -190,7 +206,7 @@ def export_pics(file,
         if filename is None:
             filename = os.path.splitext(file)[0] + '_%d.png'
         if musicname is None:
-            musicname = os.path.splitext(file)[0]
+            musicname = os.path.splitext(os.path.split(file)[1])[0]
 
         extention = os.path.splitext(file)[1]
         if extention == '.emid':
@@ -204,7 +220,7 @@ def export_pics(file,
         if filename is None:
             filename = os.path.splitext(file.filename)[0] + '_%d.png'
         if musicname is None:
-            musicname = os.path.splitext(file.filename)[0]
+            musicname = os.path.splitext(os.path.split(file)[1])[0]
 
         if typ == emid.EmidFile:
             notes, length = process_emidfile(file)
@@ -237,119 +253,151 @@ def export_pics(file,
     endpos = (size[0] / 2 + contentsize[0] / 2,
               size[1] / 2 + contentsize[1] / 2)  # 计算坐标
 
-    print('Drawing...')
-    images = []
-    draws = []
     if font is None:  # 在FONT_PATH中寻找第一个能使用的字体
         for i in FONT_PATH:
             try:
-                font0 = PIL.ImageFont.truetype(i, round(mm2pixel(3.4)))
-                font1 = PIL.ImageFont.truetype(i, round(mm2pixel(6)))
+                font0 = PIL.ImageFont.truetype(i, round(mm2pixel(3.3, ppi)))
+                font1 = PIL.ImageFont.truetype(i, round(mm2pixel(3.4, ppi)))
+                font2 = PIL.ImageFont.truetype(i, round(mm2pixel(6, ppi)))
             except:
                 pass
             else:
                 break
     else:
-        font0 = PIL.ImageFont.truetype(font, round(mm2pixel(3.4)))
-        font1 = PIL.ImageFont.truetype(font, round(mm2pixel(6)))
+        font0 = PIL.ImageFont.truetype(font, round(mm2pixel(3.3, ppi)))
+        font1 = PIL.ImageFont.truetype(font, round(mm2pixel(3.4, ppi)))
+        font2 = PIL.ImageFont.truetype(font, round(mm2pixel(6, ppi)))
 
+    print('Drawing...')
+    images0 = []
+    images1 = []
+    draws0 = []
+    draws1 = []
     for i in range(pages):
-        image = PIL.Image.new('RGBA', posconvert(size), (0, 0, 0, 0))
-        draw = PIL.ImageDraw.Draw(image)
+        image0 = PIL.Image.new('RGBA', posconvert(size, ppi), (0, 0, 0, 0))
+        image1 = PIL.Image.new('RGBA', posconvert(
+            size, ppi * ANTI_ALIAS), (0, 0, 0, 0))
+        draw0 = PIL.ImageDraw.Draw(image0)
+        draw1 = PIL.ImageDraw.Draw(image1)
         '写字'
         for j in range(col if i < pages - 1 else cols):
+            '标题文字'
+            headingtext, align = heading
+            textsize = font0.getsize(headingtext)
+
+            if align == LEFT_ALIGN:
+                posX = 7
+            elif align == CENTER_ALIGN:
+                posX = (size[0] - pixel2mm(textsize[0], ppi)) / 2
+            elif align == RIGHT_ALIGN:
+                posX = (size[0] - pixel2mm(textsize[0], ppi)) - 7
+            posY = ((size[1] - contentsize[1]) / 2 -
+                    pixel2mm(textsize[1], ppi)) - 1
+
+            draw0.text(xy=posconvert((posX, posY), ppi),
+                       text=headingtext,
+                       font=font0,
+                       fill=(0, 0, 0, 255))
             '栏尾页码'
             colnum = i * col + j + 1
-            draw.text(xy=posconvert((startpos[0] + 70*j + 6, endpos[1])),
-                      text=str(colnum),
-                      font=font0,
-                      fill=(0, 0, 0, 255))
-            '曲目标题'
+            draw0.text(xy=posconvert((startpos[0] + 70*j + 6, endpos[1]), ppi),
+                       text=str(colnum),
+                       font=font1,
+                       fill=(0, 0, 0, 255))
+            '栏右上角文字'
             for k, char in enumerate(musicname):
-                textsize = font1.getsize(char)
-                draw.text(xy=posconvert((startpos[0] + 70*j + 62 - pixel2mm(textsize[0]),
-                                         startpos[1] + 8*k - 0.7)),
-                          text=char,
-                          font=font1,
-                          fill=(0, 0, 0, 64))
+                textsize = font2.getsize(char)
+                draw0.text(
+                    xy=posconvert(
+                        (startpos[0] + 70*j + 59 - pixel2mm(textsize[0], ppi) / 2,
+                         startpos[1] + 8*k + 7 - pixel2mm(textsize[1], ppi)), ppi),
+                    text=char, font=font2, fill=(0, 0, 0, 64))
             '栏右上角页码'
-            textsize = font1.getsize(str(colnum))
-            draw.text(xy=posconvert((startpos[0] + 70*j + 62 - pixel2mm(textsize[0]),
-                                     startpos[1] + 8*len(filename) - 0.7)),
-                      text=str(colnum),
-                      font=font1,
-                      fill=(0, 0, 0, 64))
+            textsize = font2.getsize(str(colnum))
+            draw0.text(
+                xy=posconvert(
+                    (startpos[0] + 70*j + 62 - pixel2mm(textsize[0], ppi),
+                     startpos[1] + 8*len(musicname) + 7 - pixel2mm(textsize[1], ppi)), ppi),
+                text=str(colnum), font=font2, fill=(0, 0, 0, 64))
         '画格子'
         for j in range(col if i < pages - 1 else cols):
             '半拍横线'
             for k in range(row):
-                draw.line(posconvert((startpos[0] + 70*j + 6,
-                                      startpos[1] + 8*k + 4)) +
-                          posconvert((startpos[0] + 70*j + 6 + 2*29,
-                                      startpos[1] + 8*k + 4)),
-                          fill=(0, 0, 0, 80), width=1)
+                draw0.line(posconvert((startpos[0] + 70*j + 6,
+                                       startpos[1] + 8*k + 4), ppi) +
+                           posconvert((startpos[0] + 70*j + 6 + 2*29,
+                                       startpos[1] + 8*k + 4), ppi),
+                           fill=(0, 0, 0, 80), width=1)
             '整拍横线'
             for k in range(row + 1):
-                draw.line(posconvert((startpos[0] + 70*j + 6,
-                                      startpos[1] + 8*k)) +
-                          posconvert((startpos[0] + 70*j + 6 + 2*29,
-                                      startpos[1] + 8*k)),
-                          fill=(0, 0, 0, 255), width=1)
+                draw0.line(posconvert((startpos[0] + 70*j + 6,
+                                       startpos[1] + 8*k), ppi) +
+                           posconvert((startpos[0] + 70*j + 6 + 2*29,
+                                       startpos[1] + 8*k), ppi),
+                           fill=(0, 0, 0, 255), width=1)
             '竖线'
             for k in range(30):
-                draw.line(posconvert((startpos[0] + 70*j + 6 + 2*k,
-                                      startpos[1])) +
-                          posconvert((startpos[0] + 70*j + 6 + 2*k,
-                                      endpos[1])),
-                          fill=(0, 0, 0, 255), width=1)
+                draw0.line(posconvert((startpos[0] + 70*j + 6 + 2*k,
+                                       startpos[1]), ppi) +
+                           posconvert((startpos[0] + 70*j + 6 + 2*k,
+                                       endpos[1]), ppi),
+                           fill=(0, 0, 0, 255), width=1)
         '分隔线'
         for j in range(col + 1 if i < pages - 1 else cols + 1):
-            draw.line(posconvert((startpos[0] + 70*j,
-                                  startpos[1])) +
-                      posconvert((startpos[0] + 70*j,
-                                  endpos[1])),
-                      fill=(0, 0, 0, 255), width=1)
+            draw0.line(posconvert((startpos[0] + 70*j,
+                                   startpos[1]), ppi) +
+                       posconvert((startpos[0] + 70*j,
+                                   endpos[1]), ppi),
+                       fill=(0, 0, 0, 255), width=1)
 
-        images.append(image)
-        draws.append(draw)
+        images0.append(image0)
+        images1.append(image1)
+        draws0.append(draw0)
+        draws1.append(draw1)
     '画note'
     for pitch, time in notes:
         page = math.floor(time / (col * row * 8))
         coln = math.floor(time / (row * 8)) - page * col
         # math.modf(x)[0]取小数部分
         rowmm = math.modf(time / (row * 8))[0] * (row * 8)
-        draw = draws[page]
-        draw.ellipse(posconvert((startpos[0] + 70*coln + 6 + 2*pitch - DOT_R,
-                                 startpos[1] + rowmm - DOT_R)) +
-                     posconvert((startpos[0] + 70*coln + 6 + 2*pitch + DOT_R,
-                                 startpos[1] + rowmm + DOT_R)),
-                     fill=(0, 0, 0, 255))
+        draw1 = draws1[page]
+        draw1.ellipse(posconvert((startpos[0] + 70*coln + 6 + 2*pitch - DOT_R,
+                                  startpos[1] + rowmm - DOT_R), ppi * ANTI_ALIAS) +
+                      posconvert((startpos[0] + 70*coln + 6 + 2*pitch + DOT_R,
+                                  startpos[1] + rowmm + DOT_R), ppi * ANTI_ALIAS),
+                      fill=(0, 0, 0, 255))
+    print('Resizing...')
+    for i in range(pages):
+        images1[i] = images1[i].resize(posconvert(size), PIL.Image.BILINEAR)
+        images0[i] = PIL.Image.alpha_composite(images0[i], images1[i])
+        draws0[i] = PIL.ImageDraw.Draw(images0[i])
+
     '添加border'
-    for draw in draws:
-        draw.rectangle(posconvert((0, 0)) +
-                       posconvert((BORDER, size[1])),
+    for draw in draws0:
+        draw.rectangle(posconvert((0, 0), ppi) +
+                       posconvert((BORDER, size[1]), ppi),
                        fill=(255, 255, 255, 0))
-        draw.rectangle(posconvert((0, 0)) +
-                       posconvert((size[0], BORDER)),
+        draw.rectangle(posconvert((0, 0), ppi) +
+                       posconvert((size[0], BORDER), ppi),
                        fill=(255, 255, 255, 0))
-        draw.rectangle(posconvert((size[0] - BORDER, 0)) +
-                       posconvert((size[0], size[1])),
+        draw.rectangle(posconvert((size[0] - BORDER, 0), ppi) +
+                       posconvert((size[0], size[1]), ppi),
                        fill=(255, 255, 255, 0))
-        draw.rectangle(posconvert((0, size[1] - BORDER)) +
-                       posconvert((size[0], size[1])),
+        draw.rectangle(posconvert((0, size[1] - BORDER), ppi) +
+                       posconvert((size[0], size[1]), ppi),
                        fill=(255, 255, 255, 0))
     '处理background'
     if type(background) == str:
         bgimage = PIL.Image.open(background).resize(
-            (posconvert(size)), PIL.Image.BICUBIC).convert('RGBA')  # 打开，缩放，转换
+            (posconvert(size, ppi)), PIL.Image.BICUBIC).convert('RGBA')  # 打开，缩放，转换
     elif type(background) == PIL.Image.Image:
         bgimage = background.resize(
-            (posconvert(size)), PIL.Image.BICUBIC).convert('RGBA')  # 打开，缩放，转换
+            (posconvert(size, ppi)), PIL.Image.BICUBIC).convert('RGBA')  # 打开，缩放，转换
     elif type(background) == tuple:
-        bgimage = PIL.Image.new('RGBA', posconvert(size), background)
+        bgimage = PIL.Image.new('RGBA', posconvert(size, ppi), background)
     '导出图片'
     result = []
-    for pagenum, image in enumerate(images):
+    for pagenum, image in enumerate(images0):
         cpimage = PIL.Image.alpha_composite(bgimage, image)  # 拼合图像
         if save_pic:
             save_path = filename % (pagenum + 1)
@@ -398,4 +446,17 @@ if __name__ == '__main__':
     #             transposition=0,
     #             papersize=A4_VERTICAL,
     #             ppi=300)
-    batch_export_pics(overwrite=False)
+    # batch_export_pics(overwrite=False)
+    export_pics(r"D:\ChenXu\Musics\纸带八音盒\希望之花\希望之花.mid",
+                # filename='example_%d.png',
+                # musicname='希望之花gyulqWMPJj',
+                scale=1,
+                overwrite=True,
+                interpret_bpm=None,
+                save_pic=True,
+                transposition=0,
+                papersize=A4_VERTICAL,
+                # papersize=AUTO_SIZE,
+                ppi=300,
+                # font='simsun.ttc',
+                heading=('作者：BioHazard  交流群：586134350/959125559  请勿用于任何商业用途  打谱软件：github.com/BiologyHazard/MusicBoxDesigner', CENTER_ALIGN))
